87e26ca88bd0d1095dff3983b62f819eadae39a2
Sayed-Hasinoor-Alam <122892723+Sayed-Hasinoor-Alam@users.noreply.github.com>
2024-04-28 15:15:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
 require('./db/conn')
 const cors = require('cors');
-
+const port = 4009
 
 app.use(cors());
 app.use(express.json());
